453dc85ac0efdc2fc179f860079c7df1ca8d7a57
pavel_bezglasniy <pavelb23@gmail.com>
2020-01-31 15:50:28 +0300

    Change form to bootstrap.

--- src/distributions/Binomial.js
+++ src/distributions/Binomial.js
@@ -1,5 +1,7 @@
 import React from "react";
-import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from "recharts"
+import {Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis} from "recharts"
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 function nChooseK(n, k) {
     let res = 1;
@@ -16,15 +18,16 @@ class Binomial extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {p: 0.5, n: 5, data: []};
+        this.state = {binom_p: 0.5, binom_n: 10, data: []};
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.barChartElement = React.createRef();
     }
 
     handleChange(event) {
-        const {name, value} = event.target;
-        this.setState({[name]: value});
+        const id = event.target.id;
+        const value = event.target.value;
+        this.setState({[id]: value});
     }
 
 
@@ -33,10 +36,11 @@ class Binomial extends React.Component {
     }
 
     drawChart() {
-        let newData = [];
-        for (let k = 1; k <= this.state.n; k++) {
-            let choose = nChooseK(this.state.n, k);
-            let binProb = choose * Math.pow(this.state.p, k) * Math.pow(1 - this.state.p, this.state.n - k);
+        const newData = [];
+        for (let k = 1; k <= this.state.binom_n; k++) {
+            let choose = nChooseK(this.state.binom_n, k);
+            let binProb = choose * Math.pow(this.state.binom_p, k) *
+                Math.pow(1 - this.state.binom_p, this.state.binom_n - k);
             newData.push({'name': k.toString(), 'binProb': binProb});
         }
         this.setState({data: newData});
@@ -51,17 +55,27 @@ class Binomial extends React.Component {
     render() {
         return (
             <div>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Probability of success in one trial:
-                        <input type="number" value={this.state.p} name="p" onChange={this.handleChange}/>
-                    </label>
-                    <label>
-                        Num of trials:
-                        <input type="number" value={this.state.n} name="n" onChange={this.handleChange}/>
-                    </label>
-                    <input type="submit" value="Submit"/>
-                </form>
+                <div>
+                    <Form onSubmit={this.handleSubmit}>
+                        <Form.Group controlId="binom_p">
+                            <Form.Label>Probability of success in one trial:</Form.Label>
+                            <Form.Control type="input" placeholder="values from 0 to 1"
+                                          size="sm"
+                                          value={this.state.binom_p}
+                                          onChange={this.handleChange}/>
+                        </Form.Group>
+                        <Form.Group controlId="binom_n">
+                            <Form.Label>Num of trials:</Form.Label>
+                            <Form.Control type="input" placeholder="values greater than 0"
+                                          size="sm"
+                                          value={this.state.binom_n}
+                                          onChange={this.handleChange}/>
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </div>
                 <BarChart width={600} height={300} data={this.state.data}
                           margin={{top: 5, right: 30, left: 20, bottom: 5}}
                           ref={this.barChartElement}>
