2bfed474d14d7d13d9c817a9c75d500025f992a0
pavel_bezglasniy <pavelb23@gmail.com>
2020-01-31 12:49:07 +0300

    Added binomial distribution.

--- src/distributions/Binomial.js
+++ src/distributions/Binomial.js
@@ -1,11 +1,24 @@
 import React from "react";
 import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from "recharts"
 
+function nChooseK(n, k) {
+    let res = 1;
+    if (k > n - k)
+        k = n - k;
+    for (let i = 0; i < k; ++i) {
+        res *= (n - i);
+        res /= (i + 1);
+    }
+    return res;
+}
+
+//{name:k, prob:p}
+
 class Binomial extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {x: 0, n: 0, r: 0, data: [{name: 'Page A', uv: 4000, pv: 2400, amt: 2400}]};
+        this.state = {p: 0, n: 0, r: 0, data: []};
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.barChartElement = React.createRef();
@@ -23,12 +36,15 @@ class Binomial extends React.Component {
     }
 
     handleSubmit(event) {
-        let newR = this.state.x * this.state.n;
+        let newR = this.state.p * this.state.n;
         this.setState({'r': newR});
-        let newData = [...this.state.data];
+        let newData = [];
         window.console.log(newData);
-        // newData[0].pv = newData[0].pv * 2;
-        newData.push({name: 'Page A', uv: 4000, pv: 5400, amt: 4400});
+        for (let k = 1; k <= this.state.n; k++) {
+            let choose = nChooseK(this.state.n, k);
+            let binProb = choose * Math.pow(this.state.p, k) * Math.pow(1 - this.state.p, this.state.n - k);
+            newData.push({'name': k.toString(), 'binProb': binProb});
+        }
         this.setState({data: newData});
         event.preventDefault();
     }
@@ -39,16 +55,15 @@ class Binomial extends React.Component {
             <div>
                 <form onSubmit={this.handleSubmit}>
                     <label>
-                        X:
-                        <input type="number" value={this.state.x} name="x" onChange={this.handleChange}/>
+                        Probability of success in one trial:
+                        <input type="number" value={this.state.p} name="p" onChange={this.handleChange}/>
                     </label>
                     <label>
-                        N:
+                        Num of trials:
                         <input type="number" value={this.state.n} name="n" onChange={this.handleChange}/>
                     </label>
                     <input type="submit" value="Submit"/>
                 </form>
-                <p>{this.state.r}</p>
                 <BarChart width={600} height={300} data={this.state.data}
                           margin={{top: 5, right: 30, left: 20, bottom: 5}}
                           ref={this.barChartElement}>
@@ -57,8 +72,7 @@ class Binomial extends React.Component {
                     <YAxis/>
                     <Tooltip/>
                     <Legend/>
-                    <Bar dataKey="pv" fill="#8884d8"/>
-                    <Bar dataKey="uv" fill="#82ca9d"/>
+                    <Bar dataKey="binProb" fill="#8884d8"/>
                 </BarChart>
             </div>
         );
